b922f35c2989902da451184624885a48d30194d5
iamrohitsuthar <iamrohitsuthar@gmail.com>
2020-03-08 19:30:20 +0530

    Add Preloader

---
+++ js/preloader.js
@@ -0,0 +1,350 @@
+var strings = [
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+    "Enabling /etc/fstab swaps",
+    "INIT: Entering runlevel 3",
+    "Entering non-interactive startup",
+    "Applying INTEL CPU microcode update",
+    "Checking for hardware changes",
+    "Bringing up interface eth0",
+    "Determining IP information for eth0... done.",
+    "Starting autditd",
+    "Starting restorecond",
+    "Starting system logger",
+    "Starting kernel logger",
+    "Starting irqbalance",
+    "Starting mcstausd",
+    "Starting portmap",
+    "Starting setroubleshootd",
+    "Starting RPC idmapd",
+    "Starting mdmonitor",
+    "Starting system message bus",
+    "Starting Bluetooth services",
+    "Starting other filesystems",
+    "Starting PC/SC smart card daemon (pcscd)",
+    "Starting hidd",
+  ];
+  
+  var preloader = document.getElementById('preloader');
+  var delay = 1000;
+  var count = 0;
+  var repeat = 0;
+  
+  function addLog() {
+    var row = createLog('ok', count);
+    preloader.appendChild(row);
+    
+    goScrollToBottom();
+    
+    count++;
+    
+    if (repeat == 0) {
+      if (count > 3) {
+        delay = 300;
+      }
+      
+      if (count > 6) {
+        delay = 100;
+      }
+      
+      if (count > 8) {
+        delay = 50;
+      }
+      
+      if (count > 10) {
+        delay = 10;
+      }
+    } else {
+      if (count > 3) {
+        delay = 10;
+      }
+    }
+    
+    if (count < strings.length) {
+      setTimeout(function() {
+        return addLog();
+      }, delay);
+    } else {
+      if (repeat != 1) {
+        count = 0;
+        repeat++;
+  
+        setTimeout(function() {
+          delay = 1000;
+          return addLog();
+        }, 1000);
+      } else {
+        var failRow = createLog('ok');
+        preloader.appendChild(failRow);
+        goScrollToBottom();
+      }
+    }
+  }
+  
+  function createLog(type, index) {
+    var row = document.createElement('div');
+    
+    var spanStatus = document.createElement('span');
+    spanStatus.className = type;
+    spanStatus.innerHTML = type.toUpperCase();
+    
+    var message = (index != null) 
+                ? strings[index] 
+                : 'kernel: Initializing...';
+    
+    var spanMessage = document.createElement('span');
+    spanMessage.innerHTML = message;
+    
+    row.appendChild(spanStatus);
+    row.appendChild(spanMessage);
+    
+    return row;
+  }
+  
+  function goScrollToBottom() {
+    preloader.scrollTop = preloader.scrollHeight - preloader.clientHeight;
+  }
+  
+  addLog();
+  
+  
