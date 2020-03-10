a5c383e68fc61723f1e54b36ba9d0b4209a9db68
RutujaD182 <dhanawaderutuja1002@gmail.com>
2020-03-10 16:07:05 +0530

    trying to fix

--- lakshya/ctf/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Questions, Submission
-
-admin.site.register(UserProfile)
-admin.site.register(Questions)
-admin.site.register(Submission)
